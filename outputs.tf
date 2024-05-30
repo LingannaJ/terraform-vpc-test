074900e9426cc841402c670280a79dfb4a50e978
LingannaJ <jlinganna104@gmail.com>
2024-05-30 19:26:41 +0530

    vpc-test

---
+++ outputs.tf
@@ -0,0 +1,17 @@
+# output "azs" {
+#     value = module.aws_vpc.azs
+# }
+
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "public_subnet_list" {
+  value = module.vpc.public_subnet_ids
+}
+
+output "igw_id" {
+  value = module.vpc.igw_id
+}
+
